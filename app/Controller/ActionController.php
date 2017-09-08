f6cae450a7978e40f54ec309d0b2619ee792aee5
Hai TM <haitm@leverages.jp>
2017-09-08 09:01:12 +0700

    .

--- app/Controller/ActionController.php
+++ app/Controller/ActionController.php
@@ -1049,7 +1049,7 @@ class ActionController extends AppController
             $func = 'calculate_' . mb_strtolower($model);
             $tmp = $this->$func($model);
             foreach ($tmp as $provice_code => $result) {
-                $export[$provice_code] = array_merge($export[$provice_code], $result);
+                $export[$provice_code] = array_merge($export[$provice_code], array_values($result));
             }
         }
         print_r('<pre>');
