c9ffe6754bc6cbdc32eac4169e3a809221016d0d
haule <hauln@leverages.jp>
2018-05-21 13:00:20 +0700

    DANH SÁCH CƠ SỞ THỜ TỰ TÔN GIÁO, TÍN NGƯỠNG TRÊN ĐỊA BÀN TỈNH

--- app/Controller/ActionController.php
+++ app/Controller/ActionController.php
@@ -1033,6 +1033,334 @@ class ActionController extends AppController
 
     /**
      * DANH SÁCH CƠ SỞ THỜ TỰ TÔN GIÁO, TÍN NGƯỠNG TRÊN ĐỊA BÀN TỈNH
+     * 
+     * I. CÔNG GIÁO
+     * 1. Bảng giaoxu
+     * Tên cơ sở thờ tự: tengiaoxu
+     * Địa chỉ: diachi_so, diachi_duong, diachi_ap, diachi_xa, diachi_huyen, diachi_tinh
+     * Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Công Giáo'
+     * Số lượng tín đồ
+     *      Đã thực hiện lễ nghi tôn giáo: giaodan_sonhankhau
+     *      Chưa thực hiện lễ nghi tôn giáo: giaodan_sonhankhau
+     *      Là người dân tộc thiểu số: giaodandantocthieuso_sonhankhau
+     * Số lượng chức sắc tu sĩ
+     *      Chức sắc: Để mặc định bằng 0
+     *      Tu sĩ: sotusi
+     *      Chắc sắc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức việc
+     *      Số lượng chức việc: sothanhvientrongthuongvu
+     *      Số lượng chức việc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Năm thành lập: namthanhlap
+     * Về xây dựng
+     *      Năm xây dựng: namxaydungnhatho
+     *      Số tiền: Để mặc định bằng 0
+     * Về trùng tu tôn tạo
+     *      Số lần: ttttcs_solan
+     *      Số tiền lần cuối: ttttcs_tongkinhphi
+     * Diện tích (m2)
+     *      Đã cấp GCNQSD đất
+     *          Mục đích sử dụng đất TG: 
+     *          Mục đích sử dụng khác: 
+     *      Chưa được cấp GCNQSD đất
+     * Kiến trúc: 
+     * Di tích
+     *      Xếp hạng di tích: 
+     *          if cosothotu_ditichlichsu == true
+     *              Xếp hạng di tích: 'Di tích lịch sử'
+     *          if cosothotu_ditichvanhoa == true
+     *              Xếp hạng di tích: 'Di tích văn hóa'
+     *          if cosothotu_ditichlichsuvanhoa == true
+     *              Xếp hạng di tích: 'Di tích lịch sử văn hóa'
+     *          if cosothotu_ditichkientrucnghethuat == true
+     *              Xếp hạng di tích: 'Di tích kiến trúc nghệ thuật'
+     *          if cosothotu_ditichkhaoco == true
+     *              Xếp hạng di tích: 'Di tích khảo cổ'
+     *      Cấp công nhận
+     *          if cosothotu_captrunguong == true
+     *              Cấp công nhận: 'Cấp Trung ương'
+     *          if cosothotu_captinh == true
+     *              Cấp công nhận: 'Cấp tỉnh'
+     *
+     * II. PHẬT GIÁO 
+     * 2. Bảng tuvienphatgiao
+     * Tên cơ sở thờ tự: tentuvien
+     * Địa chỉ: diachi_so, diachi_duong, diachi_ap, diachi_xa, diachi_huyen, diachi_tinh
+     * Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Phật Giáo'
+     * Số lượng tín đồ
+     *      Đã thực hiện lễ nghi tôn giáo: daquyy
+     *      Chưa thực hiện lễ nghi tôn giáo: soluongtindo - daquyy
+     *      Là người dân tộc thiểu số: phattu_dantoc_thieuso
+     * Số lượng chức sắc tu sĩ
+     *      Chức sắc: sochucsac
+     *      Tu sĩ: sotusi
+     *      Chắc sắc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức việc
+     *      Số lượng chức việc: Để mặc định bằng 0
+     *      Số lượng chức việc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Năm thành lập: namthanhlap
+     * Về xây dựng
+     *      Năm xây dựng: namxaydung
+     *      Số tiền: Để mặc định bằng 0
+     * Về trùng tu tôn tạo
+     *      Số lần: ttttcs_solan
+     *      Số tiền lần cuối: ttttcs_tongkinhphi
+     * Diện tích (m2)
+     *      Đã cấp GCNQSD đất
+     *          Mục đích sử dụng đất TG: 
+     *          Mục đích sử dụng khác: 
+     *      Chưa được cấp GCNQSD đất
+     * Kiến trúc: 
+     * Di tích
+     *      Xếp hạng di tích: 
+     *          if cosothotu_ditichlichsu == true
+     *              Xếp hạng di tích: 'Di tích lịch sử'
+     *          if cosothotu_ditichvanhoa == true
+     *              Xếp hạng di tích: 'Di tích văn hóa'
+     *          if cosothotu_ditichlichsuvanhoa == true
+     *              Xếp hạng di tích: 'Di tích lịch sử văn hóa'
+     *          if cosothotu_ditichkientrucnghethuat == true
+     *              Xếp hạng di tích: 'Di tích kiến trúc nghệ thuật'
+     *          if cosothotu_ditichkhaoco == true
+     *              Xếp hạng di tích: 'Di tích khảo cổ'
+     *      Cấp công nhận
+     *          if cosothotu_captrunguong == true
+     *              Cấp công nhận: 'Cấp Trung ương'
+     *          if cosothotu_captinh == true
+     *              Cấp công nhận: 'Cấp tỉnh'
+     * III. TIN LÀNH
+     * 3. Bảng chihoitinlanh
+     * Tên cơ sở thờ tự: tenchihoi
+     * Địa chỉ: diachi_so, diachi_ap, diachi_xa, diachi_huyen, diachi_tinh
+     * Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Tin Lành'
+     * Số lượng tín đồ
+     *      Đã thực hiện lễ nghi tôn giáo: tongsotindo_baptem
+     *      Chưa thực hiện lễ nghi tôn giáo: tongsotindo_chuabaptem
+     *      Là người dân tộc thiểu số: sotindo_dantoc_thieuso
+     * Số lượng chức sắc tu sĩ
+     *      Chức sắc: sochucsac
+     *      Tu sĩ: Để mặc định bằng 0
+     *      Chắc sắc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức việc
+     *      Số lượng chức việc: sothanhvientrongbanchapsu
+     *      Số lượng chức việc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Năm thành lập: namgiaohoithanhlap
+     * Về xây dựng
+     *      Năm xây dựng: ttttcs_namxaydung
+     *      Số tiền: Để mặc định bằng 0
+     * Về trùng tu tôn tạo
+     *      Số lần: ttttcs_solan
+     *      Số tiền lần cuối: ttttcs_tongkinhphi
+     * Diện tích (m2)
+     *      Đã cấp GCNQSD đất
+     *          Mục đích sử dụng đất TG: 
+     *          Mục đích sử dụng khác: 
+     *      Chưa được cấp GCNQSD đất
+     * Kiến trúc: 
+     * Di tích
+     *      Xếp hạng di tích: 
+     *          if cosothotu_ditichlichsu == true
+     *              Xếp hạng di tích: 'Di tích lịch sử'
+     *          if cosothotu_ditichvanhoa == true
+     *              Xếp hạng di tích: 'Di tích văn hóa'
+     *          if cosothotu_ditichlichsuvanhoa == true
+     *              Xếp hạng di tích: 'Di tích lịch sử văn hóa'
+     *          if cosothotu_ditichkientrucnghethuat == true
+     *              Xếp hạng di tích: 'Di tích kiến trúc nghệ thuật'
+     *          if cosothotu_ditichkhaoco == true
+     *              Xếp hạng di tích: 'Di tích khảo cổ'
+     *      Cấp công nhận
+     *          if cosothotu_captrunguong == true
+     *              Cấp công nhận: 'Cấp Trung ương'
+     *          if cosothotu_captinh == true
+     *              Cấp công nhận: 'Cấp tỉnh'
+     * IV. CAO ĐÀI
+     * 4. Bảng hodaocaodai
+     * Tên cơ sở thờ tự: tenhodao
+     * Địa chỉ: tenhodao_diachi_so, tenhodao_diachi_duong, tenhodao_diachi_ap, tenhodao_diachi_xa, tenhodao_diachi_huyen, tenhodao_diachi_tinh
+     * Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Cao Đài'
+     * Số lượng tín đồ
+     *      Đã thực hiện lễ nghi tôn giáo: tongsotindo_cosocaudao
+     *      Chưa thực hiện lễ nghi tôn giáo: tongsotindo_chuacosocaudao
+     *      Là người dân tộc thiểu số: sotindo_dantoc_thieuso
+     * Số lượng chức sắc tu sĩ
+     *      Chức sắc: sochucsac_phoisu + sochucsac_giaosu + sochucsac_giaohuu + sochucsac_lesanh
+     *      Tu sĩ: Để mặc định bằng 0
+     *      Chắc sắc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức việc
+     *      Số lượng chức việc: sothanvien_bancaiquan
+     *      Số lượng chức việc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Năm thành lập: namhoithanhthanhlap
+     * Về xây dựng
+     *      Năm xây dựng: ttttcs_namxaydung
+     *      Số tiền: Để mặc định bằng 0
+     * Về trùng tu tôn tạo
+     *      Số lần: ttttcs_solan
+     *      Số tiền lần cuối: ttttcs_tongkinhphi
+     * Diện tích (m2)
+     *      Đã cấp GCNQSD đất
+     *          Mục đích sử dụng đất TG: 
+     *          Mục đích sử dụng khác: 
+     *      Chưa được cấp GCNQSD đất
+     * Kiến trúc: 
+     * Di tích
+     *      Xếp hạng di tích: 
+     *          if cosothotu_ditichlichsu == true
+     *              Xếp hạng di tích: 'Di tích lịch sử'
+     *          if cosothotu_ditichvanhoa == true
+     *              Xếp hạng di tích: 'Di tích văn hóa'
+     *          if cosothotu_ditichlichsuvanhoa == true
+     *              Xếp hạng di tích: 'Di tích lịch sử văn hóa'
+     *          if cosothotu_ditichkientrucnghethuat == true
+     *              Xếp hạng di tích: 'Di tích kiến trúc nghệ thuật'
+     *          if cosothotu_ditichkhaoco == true
+     *              Xếp hạng di tích: 'Di tích khảo cổ'
+     *      Cấp công nhận
+     *          if cosothotu_captrunguong == true
+     *              Cấp công nhận: 'Cấp Trung ương'
+     *          if cosothotu_captinh == true
+     *              Cấp công nhận: 'Cấp tỉnh'
+     *
+     * V. TĐCSPHVN
+     * 5. Bảng chihoitinhdocusiphatgiaovietnam
+     * Tên cơ sở thờ tự: tenchihoi
+     * Địa chỉ: tenchihoi_diachi_so, tenchihoi_diachi_duong, tenchihoi_diachi_ap, tenchihoi_diachi_xa, tenchihoi_diachi_huyen, tenchihoi_diachi_tinh
+     * Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Tịnh độ cư sĩ phật hội'
+     * Số lượng tín đồ
+     *      Đã thực hiện lễ nghi tôn giáo: soluonghoivien_tindo
+     *      Chưa thực hiện lễ nghi tôn giáo: soluonghoivien_tindo
+     *      Là người dân tộc thiểu số: sotindo_dantoc_thieuso
+     * Số lượng chức sắc tu sĩ
+     *      Chức sắc: Để mặc định bằng 0
+     *      Tu sĩ: Để mặc định bằng 0
+     *      Chắc sắc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức việc
+     *      Số lượng chức việc: Để mặc định bằng 0
+     *      Số lượng chức việc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Năm thành lập: namhoithanhthanhlap
+     * Về xây dựng
+     *      Năm xây dựng: ttttcs_namxaydung
+     *      Số tiền: Để mặc định bằng 0
+     * Về trùng tu tôn tạo
+     *      Số lần: ttttcs_solan
+     *      Số tiền lần cuối: ttttcs_tongkinhphi
+     * Diện tích (m2)
+     *      Đã cấp GCNQSD đất
+     *          Mục đích sử dụng đất TG: 
+     *          Mục đích sử dụng khác: 
+     *      Chưa được cấp GCNQSD đất
+     * Kiến trúc: 
+     * Di tích
+     *      Xếp hạng di tích: 
+     *          if cosothotu_ditichlichsu == true
+     *              Xếp hạng di tích: 'Di tích lịch sử'
+     *          if cosothotu_ditichvanhoa == true
+     *              Xếp hạng di tích: 'Di tích văn hóa'
+     *          if cosothotu_ditichlichsuvanhoa == true
+     *              Xếp hạng di tích: 'Di tích lịch sử văn hóa'
+     *          if cosothotu_ditichkientrucnghethuat == true
+     *              Xếp hạng di tích: 'Di tích kiến trúc nghệ thuật'
+     *          if cosothotu_ditichkhaoco == true
+     *              Xếp hạng di tích: 'Di tích khảo cổ'
+     *      Cấp công nhận
+     *          if cosothotu_captrunguong == true
+     *              Cấp công nhận: 'Cấp Trung ương'
+     *          if cosothotu_captinh == true
+     *              Cấp công nhận: 'Cấp tỉnh'
+     *
+     * VI. HỒI GIÁO
+     * 6. Bảng cosohoigiaoislam
+     * Tên cơ sở thờ tự: tenthanhduong
+     * Địa chỉ: tenthanhduong_diachi_so, tenthanhduong_diachi_duong, tenthanhduong_diachi_ap, tenthanhduong_diachi_xa, tenthanhduong_diachi_huyen, tenthanhduong_diachi_tinh
+     * Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Hồi Giáo'
+     * Số lượng tín đồ
+     *      Đã thực hiện lễ nghi tôn giáo: tongsotindo
+     *      Chưa thực hiện lễ nghi tôn giáo: tongsotindo
+     *      Là người dân tộc thiểu số: sotindo_dantoc_thieuso
+     * Số lượng chức sắc tu sĩ
+     *      Chức sắc: Để mặc định bằng 0
+     *      Tu sĩ: Để mặc định bằng 0
+     *      Chắc sắc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức việc
+     *      Số lượng chức việc: Để mặc định bằng 0
+     *      Số lượng chức việc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Năm thành lập: namgiaohoithanhlap
+     * Về xây dựng
+     *      Năm xây dựng: ttttcs_namxaydung
+     *      Số tiền: Để mặc định bằng 0
+     * Về trùng tu tôn tạo
+     *      Số lần: ttttcs_solan
+     *      Số tiền lần cuối: ttttcs_tongkinhphi
+     * Diện tích (m2)
+     *      Đã cấp GCNQSD đất
+     *          Mục đích sử dụng đất TG: 
+     *          Mục đích sử dụng khác: 
+     *      Chưa được cấp GCNQSD đất
+     * Kiến trúc: 
+     * Di tích
+     *      Xếp hạng di tích: 
+     *          if cosothotu_ditichlichsu == true
+     *              Xếp hạng di tích: 'Di tích lịch sử'
+     *          if cosothotu_ditichvanhoa == true
+     *              Xếp hạng di tích: 'Di tích văn hóa'
+     *          if cosothotu_ditichlichsuvanhoa == true
+     *              Xếp hạng di tích: 'Di tích lịch sử văn hóa'
+     *          if cosothotu_ditichkientrucnghethuat == true
+     *              Xếp hạng di tích: 'Di tích kiến trúc nghệ thuật'
+     *          if cosothotu_ditichkhaoco == true
+     *              Xếp hạng di tích: 'Di tích khảo cổ'
+     *      Cấp công nhận
+     *          if cosothotu_captrunguong == true
+     *              Cấp công nhận: 'Cấp Trung ương'
+     *          if cosothotu_captinh == true
+     *              Cấp công nhận: 'Cấp tỉnh'
+     * 
+     * VII. TÍN NGƯỠNG
+     * 6. Bảng cosotinnguong
+     * Tên cơ sở thờ tự: tencoso
+     * Địa chỉ: diachi_so, diachi_duong, diachi_ap, diachi_xa, diachi_huyen, diachi_tinh
+     * Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Tín Ngưỡng'
+     * Số lượng tín đồ
+     *      Đã thực hiện lễ nghi tôn giáo: Để mặc định bằng 0
+     *      Chưa thực hiện lễ nghi tôn giáo: Để mặc định bằng 0
+     *      Là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức sắc tu sĩ
+     *      Chức sắc: Để mặc định bằng 0
+     *      Tu sĩ: Để mặc định bằng 0
+     *      Chắc sắc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Số lượng chức việc
+     *      Số lượng chức việc: Để mặc định bằng 0
+     *      Số lượng chức việc là người dân tộc thiểu số: Để mặc định bằng 0
+     * Năm thành lập: namthanhlap
+     * Về xây dựng
+     *      Năm xây dựng: namxaydung
+     *      Số tiền: Để mặc định bằng 0
+     * Về trùng tu tôn tạo
+     *      Số lần: ttttcs_solan
+     *      Số tiền lần cuối: ttttcs_tongkinhphi
+     * Diện tích (m2)
+     *      Đã cấp GCNQSD đất
+     *          Mục đích sử dụng đất TG: 
+     *          Mục đích sử dụng khác: 
+     *      Chưa được cấp GCNQSD đất
+     * Kiến trúc: 
+     * Di tích
+     *      Xếp hạng di tích: 
+     *          if cosothotu_ditichlichsu == true
+     *              Xếp hạng di tích: 'Di tích lịch sử'
+     *          if cosothotu_ditichvanhoa == true
+     *              Xếp hạng di tích: 'Di tích văn hóa'
+     *          if cosothotu_ditichlichsuvanhoa == true
+     *              Xếp hạng di tích: 'Di tích lịch sử văn hóa'
+     *          if cosothotu_ditichkientrucnghethuat == true
+     *              Xếp hạng di tích: 'Di tích kiến trúc nghệ thuật'
+     *          if cosothotu_ditichkhaoco == true
+     *              Xếp hạng di tích: 'Di tích khảo cổ'
+     *      Cấp công nhận
+     *          if cosothotu_captrunguong == true
+     *              Cấp công nhận: 'Cấp Trung ương'
+     *          if cosothotu_captinh == true
+     *              Cấp công nhận: 'Cấp tỉnh'
+     *
      */
     protected function __getType8Data()
     {
@@ -1113,7 +1441,34 @@ class ActionController extends AppController
                 Cơ quan công nhận: cachoatdongbacai_coquancap
                 Giấy chứng nhận: cachoatdongbacai_sogiaychungnhan
                 Ghi chú: Bỏ trống
+     * VI. HỒI GIÁO
+     * 6. Bảng cosohoigiaoislam
+     * Tên cơ sở thờ tự: tenthanhduong
+       Địa chỉ: tenthanhduong_diachi_so, tenthanhduong_diachi_duong, tenthanhduong_diachi_ap, tenthanhduong_diachi_xa, tenthanhduong_diachi_huyen, tenthanhduong_diachi_tinh
+       Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Hồi Giáo'
+       Lấy dữ liệu từ cột cachoatdongbacai, dữ liệu lưu như dưới có thể n+ (n = 1, 2, 3, ...)
+            cachoatdongbacai_linhvuchoatdong______:Lĩnh vực hoạt động 1______,cachoatdongbacai_sogiaychungnhan______:GCN1______,cachoatdongbacai_ngaycap______:11/11/2011______,cachoatdongbacai_coquancap______:CQC1
+                Sau khi parse xong, lấy dữ liệu excel như sau:
+                Thuộc lĩnh vực: cachoatdongbacai_linhvuchoatdong
+                Cơ sở họp pháp/chưa hợp pháp: if cachoatdongbacai_capdangkyhoatdong = true Cơ sở họp pháp else chưa hợp pháp
+                Cơ quan công nhận: cachoatdongbacai_coquancap
+                Giấy chứng nhận: cachoatdongbacai_sogiaychungnhan
+                Ghi chú: Bỏ trống
      *
+     * VII. TÍN NGƯỠNG
+     * 7. Bảng cosotinnguong
+     * Tên cơ sở thờ tự: tencoso
+       Địa chỉ: diachi_so, diachi_duong, diachi_ap, diachi_xa, diachi_huyen, diachi_tinh
+       Thuộc tổ chức tôn giáo cơ sở: Để mặc định là 'Tín Ngưỡng'
+       Lấy dữ liệu từ cột cachoatdongbacai, dữ liệu lưu như dưới có thể n+ (n = 1, 2, 3, ...)
+            cachoatdong_linhvuchoatdong______:Lĩnh vực hoạt động 1______,cachoatdong_sogiaychungnhan______:GCN 1______,cachoatdong_sogiaychungnhan_ngaycap______:11//11/2011______,cachoatdong_sogiaychungnhan_coquancap______:CQC1
+                Sau khi parse xong, lấy dữ liệu excel như sau:
+                Thuộc lĩnh vực: cachoatdong_linhvuchoatdong
+                Cơ sở họp pháp/chưa hợp pháp: if cachoatdongbacai_capdangkyhoatdong = true Cơ sở họp pháp else chưa hợp pháp
+                Cơ quan công nhận: cachoatdong_sogiaychungnhan_coquancap
+                Giấy chứng nhận: cachoatdong_sogiaychungnhan
+                Ghi chú: Bỏ trống
+                
      * Đây là phải excel lấy danh sách, nên chỉ cần lấy dữ liệu trả về mảng như định dạng sau là được
      *      $array = array (
      *          0 => array (
