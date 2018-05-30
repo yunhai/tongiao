bd8fdde6348f93b53fbfab1169b37a574f774d20
Hai TM <haitm@leverages.jp>
2018-05-30 16:37:57 +0700

    TỔNG HỢP CHỨC SẮC TÔN GIÁO THAM GIA CÁC TỔ CHỨC CHÍNH TRỊ - XÃ HỘI CẤP TỈNH

--- app/Controller/ActionController.php
+++ app/Controller/ActionController.php
@@ -3272,7 +3272,7 @@ class ActionController extends AppController
     protected function __getType16Data($conditions)
     {
         $component = $this->Components->load('ExportThCtxhTinh');
-        $data = $component->export();
+        $data = $component->export($conditions);
 
         $source = WWW_ROOT . 'files' . DS . 'templates' . DS . 'template15.xls';
         $filename = "{$this->_type_text[15]}";
