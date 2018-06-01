44514e9f849a0edbc590d17d89f1eac8415c6158
Hai TM <haitm@leverages.jp>
2018-06-01 16:01:52 +0700

    TỔNG HỢP TRÌNH ĐỘ VĂN HÓA CỦA CHỨC SẮC CÁC TÔN GIÁO

--- app/Controller/Component/ExportThTdVhCsComponent.php
+++ app/Controller/Component/ExportThTdVhCsComponent.php
@@ -1,11 +1,13 @@
 <?php
 
-class ExportThTdVhCsComponent extends Component
+App::uses('ExportExcelComponent', 'Controller/Component');
+
+
+class ExportThTdVhCsComponent extends ExportExcelComponent
 {
     public function __construct()
     {
-        App::uses('ProvinceComponent', 'Controller/Component');
-        $this->Province = new ProvinceComponent(new ComponentCollection());
+        parent::__construct();
 
         $this->map_field = [
             1 => 'tieu_hoc',
@@ -23,7 +25,7 @@ class ExportThTdVhCsComponent extends Component
 
     public function export($filter = [])
     {
-        $export_fields = [
+        $groups = [
             CONG_GIAO => [
                 'Chucsacnhatuhanhconggiaotrieu',
                 'Chucsacnhatuhanhcongiaodongtu'
@@ -61,7 +63,7 @@ class ExportThTdVhCsComponent extends Component
 
         $export = $this->init($province);
 
-        foreach ($export_fields as $field_index => $list) {
+        foreach ($groups as $field_index => $list) {
             if (!empty($filter_group) && !in_array($field_index, $filter_group)) {
                 continue;
             }
@@ -86,28 +88,31 @@ class ExportThTdVhCsComponent extends Component
         return $this->sum($export);
     }
 
-    private function sum($data, $start = 2)
+    public function layout($filter = [])
     {
-        $total = [];
+        $row_header_index = 5;
+        $row_data_index = 10;
+        $column_begin = 12;
+        $column_structure = [
+            CONG_GIAO => 8,
+            PHAT_GIAO => 8,
+            TIN_LANH => 8,
+            CAO_DAI => 8,
+            TINH_DO_CU_SI => 8,
+            HOI_GIAO => 8
+        ];
 
-        foreach ($data as $location => $target) {
-            $index = 0;
-            foreach ($target as $field => $value) {
-                if (++$index <= $start) {
-                    $total["final_total_{$field}"] = '';
-
-                    continue;
+        $column_remove = [];
+        $cell_total_count = 2;
+        if ($filter) {
+            foreach ($column_structure as $index => $tmp) {
+                if (!in_array($index, $filter)) {
+                    $column_remove[$index] = $index;
                 }
-
-                $total["final_total_{$field}"] = isset($total["final_total_{$field}"]) ? $total["final_total_{$field}"] : 0;
-                $total["final_total_{$field}"] += $value;
             }
         }
-        $data['final_total'] = $total;
-
-        return $data;
+        return compact('column_begin', 'column_structure', 'column_remove', 'row_header_index', 'row_data_index', 'cell_total_count');
     }
-
 
     private function __getChucsacnhatuhanhconggiaotrieu($model)
     {
